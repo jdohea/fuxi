172b4d85e016f3b6c31b760c8f8353c43645f0c9
celineodding <celineodding@gmail.com>
2023-04-05 17:43:27 +0200

    dcn and dcnv2

--- demo/DCNv2_run_experiment_full_h5.py
+++ demo/DCNv2_run_experiment_full_h5.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('/')
 import os
 import logging
@@ -10,11 +11,10 @@ from fuxictr.pytorch.torch_utils import seed_everything
 from fuxictr.pytorch.dataloaders import H5DataLoader
 from model_zoo import DCNv2
 
-
 if __name__ == '__main__':
     # Load params from config files
     config_dir = './config/DCNv2_h5_config'
-    experiment_id = 'full_h5_DCNv2' # corresponds to h5 input `data/tiny_h5`
+    experiment_id = 'full_h5_DCNv2'  # corresponds to h5 input `data/tiny_h5`
     params = load_config(config_dir, experiment_id)
 
     # set up logger and random seed
@@ -28,10 +28,10 @@ if __name__ == '__main__':
     feature_map = FeatureMap(params['dataset_id'], data_dir)
     feature_map.load(feature_map_json, params)
     logging.info("Feature specs: " + print_to_json(feature_map.features))
-    
+
     # Get train and validation data generators from h5
-    train_gen, valid_gen = H5DataLoader(feature_map, 
-                                        stage='train', 
+    train_gen, valid_gen = H5DataLoader(feature_map,
+                                        stage='train',
                                         train_data=params['train_data'],
                                         valid_data=params['valid_data'],
                                         batch_size=params['batch_size'],
@@ -40,12 +40,12 @@ if __name__ == '__main__':
     # Model initialization and fitting
     model = DCNv2(feature_map, **params)
     model.fit(train_gen, validation_data=valid_gen, epochs=params['epochs'])
-    
+
     logging.info('***** Validation evaluation *****')
     model.evaluate(valid_gen)
 
     logging.info('***** Test evaluation *****')
-    test_gen = H5DataLoader(feature_map, 
+    test_gen = H5DataLoader(feature_map,
                             stage='test',
                             test_data=params['test_data'],
                             batch_size=params['batch_size'],

461a24555a2194699f8b2f3fee17ec013c49b199
jdohea <jdohea@gmail.com>
2023-05-11 09:39:34 +0200

    update for deployment, data transformation now in full_h5

--- fuxictr/pytorch/models/base_model.py
+++ fuxictr/pytorch/models/base_model.py
@@ -66,7 +66,7 @@ class BaseModel(nn.Module):
     def add_loss(self, inputs):
         return_dict = self.forward(inputs)
         y_true = self.get_labels(inputs)
-        weight = y_true.clone().detach()
+        weight = y_true.clone()
         weight[y_true == 0] = 200.0
         loss = self.loss_fn(return_dict["y_pred"], y_true, reduction='mean', weight=weight)
         return loss
